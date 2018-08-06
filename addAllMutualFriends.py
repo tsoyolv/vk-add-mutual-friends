e7d5855cae6e29174b53d84757dba3a879a0d84f
Цой Олег Вячеславович <OVTsoy.OUT@sberbank.ru>
2018-08-06 11:35:20 +0300

    added normal logging

--- addAllMutualFriends.py
+++ addAllMutualFriends.py
@@ -1,58 +1,63 @@
+import logging
+import os
+from datetime import datetime
+from datetime import timedelta
 import vk_api
 import time
 import random
-import datetime
 import requests
 import urllib3
-import os
 
-OUTGOING_REQUESTS_DAY_LIMIT = 40 
-outgoingRequestsCnt = 0
-currentDay = datetime.datetime.today()
-nextDay = currentDay + datetime.timedelta(days=1)
+def LOG_PATH():
+	return 'logs'
+def FRINEDS_BE_ADDED_LOG_PATH() :
+	return os.path.join(LOG_PATH(), 'FriendsThatWillBeAdded')
+def FRIENDS_WERE_ADDED_LOG_PATH() :
+	return os.path.join(LOG_PATH(), 'FriendsThatWereAdded')
 
+def createLogFiles() :
+	if not os.path.exists(LOG_PATH()):
+		os.makedirs(LOG_PATH())		
+	if not os.path.exists(FRINEDS_BE_ADDED_LOG_PATH()):
+		os.makedirs(FRINEDS_BE_ADDED_LOG_PATH())	
+	if not os.path.exists(FRIENDS_WERE_ADDED_LOG_PATH()):
+		os.makedirs(FRIENDS_WERE_ADDED_LOG_PATH())
+	return
+
+def createLogger(name, path, level, consoleLogAdded) : 
+	logger = logging.getLogger(name)
+	logger.setLevel(logging.DEBUG)
+	ch = logging.FileHandler(os.path.join(path, name + '_{:%I-%M-%S__%d-%m-%Y}.log'.format(datetime.now())), mode='a', encoding='utf-8', delay=False)
+	ch.setLevel(level)
+	formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%m-%Y %I:%M:%S')
+	ch.setFormatter(formatter)
+	logger.addHandler(ch)	
+	if consoleLogAdded :
+		ch = logging.StreamHandler()
+		ch.setLevel(level)
+		formatter = logging.Formatter(fmt='%(asctime)s - %(message)s', datefmt='%d-%m-%Y %I:%M:%S')
+		ch.setFormatter(formatter)
+		logger.addHandler(ch)
+	return logger
+	
 def loginAndGetApi(login, password) :
 	vk_session = vk_api.VkApi(login, password)
 	vk_session.auth()
-	return vk_session.get_api()
-	
-FRIENDS_WILL_BE_CONST = 'friendsWillBeAddedstr'
-FRIENDS_WERE_ADDED_CONST ='friendsWereAddedstr'
-def createLogFiles() :
-	logsPath = os.path.join(os.getcwd(), 'logs')
-	if not os.path.exists(logsPath):
-		os.makedirs(logsPath)
-		
-	needPath = os.path.join(logsPath, 'FriendsThatWillBeAdded')
-	if not os.path.exists(needPath):
-		os.makedirs(needPath)
-		
-	addedPath = os.path.join(logsPath, 'FriendsThatWereAdded')
-	if not os.path.exists(addedPath):
-		os.makedirs(addedPath)
-	
-	datetimestr = datetime.datetime.now().strftime('_%H-%M-%m__%d-%m-%y')
-	
-	friendsWillBeAddedstr = os.path.join(needPath, 'friendsWillBeAdded' + datetimestr + '.log')
-	friendsWereAddedstr = os.path.join(addedPath, 'friendsWereAdded' + datetimestr + '.log')
-	
-	with open(friendsWillBeAddedstr, 'w+', encoding='utf-8') as file:
-		print('friendsWillBeAdded log file created.')
-	
-	with open(friendsWereAddedstr, 'w+', encoding='utf-8') as file:
-		print('friendsWereAdded log file created.')	
-		
-	return {FRIENDS_WILL_BE_CONST:friendsWillBeAddedstr, FRIENDS_WERE_ADDED_CONST: friendsWereAddedstr}
+	return vk_session.get_api()	
 
 def filterFriends(friends, vk, sex) :
-	friendsTemp = friends.copy()
-	for key in list(friendsTemp) :
+	filtered = {}
+	for key in list(friends) :
 		addFriend = vk.users.get(user_id=key,fields='sex')
-		if addFriend[0].get('sex') != sex or addFriend[0].get('deactivated') :  # gender. 1 = female, 2 = male
-			del friendsTemp[key]
-	return friendsTemp
+		if addFriend[0].get('sex') == sex and not addFriend[0].get('deactivated') :  # gender. 1 = female, 2 = male
+			filtered[key] = (addFriend, friends[key])
+	return filtered
 	
 def addPossibleFriendsWithCommonFriends(vk, login, password, mutualFriendsCnt) :
+	# new logger in every bunch
+	logWillbeAdded = createLogger('friendsWillBeAdded' + str(mutualFriendsCnt), FRINEDS_BE_ADDED_LOG_PATH(), logging.DEBUG, False)
+	logWereAdded = createLogger('friendsWereAdded'+ str(mutualFriendsCnt), FRIENDS_WERE_ADDED_LOG_PATH(), logging.INFO, True)
+	
 	MUTUAL_FRIENDS_LIMIT = mutualFriendsCnt
 
 	dictMutualFriends = {}
@@ -61,15 +66,15 @@ def addPossibleFriendsWithCommonFriends(vk, login, password, mutualFriendsCnt) :
 	friends = vk.friends.get().get('items')
 	me = vk.users.get()[0].get('id')
 	outgoingRequests = vk.friends.getRequests(out=1).get('items')
-
-	print('start processing...')
+	
+	logger.debug('start processing...')
+	
 	for friend in friends :
 		try :
 			mutualFriends = vk.friends.get(user_id=friend).get('items')
-		except vk_api.exceptions.ApiError :
-			print('\texception. Maybe deleted user with id: ' + str(friend))
+		except vk_api.exceptions.ApiError as exp :
+			logger.exception('User was deleted or banned user with id: %s', str(friend))
 			continue
-		#print('My Friend is: ' + str(vk.users.get(user_id=friend)) + '\n')
 		for mFriend in mutualFriends : 
 			if (mFriend not in friends) and (mFriend not in outgoingRequests) and (mFriend != me) :
 				if dictMutualFriends.get(mFriend) :
@@ -79,112 +84,117 @@ def addPossibleFriendsWithCommonFriends(vk, login, password, mutualFriendsCnt) :
 				else :
 					dictMutualFriends[mFriend] = 1
 
-	print('end processing')
+	logger.debug('end processing...')
 	
-	mutualFriendsForAddingTemp = filterFriends(mutualFriendsForAdding, vk, 1)
-	if len(mutualFriendsForAddingTemp) == 0 :
-		mutualFriendsForAddingTemp = mutualFriendsForAdding
-		print('switch to ANY GENDER!')
-	mutualFriendsForAdding = mutualFriendsForAddingTemp
+	mutualFriendsForAdding = filterFriends(mutualFriendsForAdding, vk, 1)
 	
-	print('friends amount: ' + str(len(mutualFriendsForAdding)))
+	logger.debug('start writing to file FriendsThatWillBeAdded...')
+	logWillbeAdded.debug('friends amount: %s. mutual friends limit = %s', str(len(mutualFriendsForAdding)), str(MUTUAL_FRIENDS_LIMIT))
+	for key in mutualFriendsForAdding :	
+		try :
+			friend = mutualFriendsForAdding[key]
+			logWillbeAdded.debug('FRIEND. cnt = %s. Info: %s', str(friend[1]), str(friend[0]))
+		except (requests.exceptions.ConnectionError, urllib3.exceptions.MaxRetryError, urllib3.exceptions.NewConnectionError) as exp :	
+			waitReconnect = random.randint(3, 5)
+			logger.exception('Connection refused! User: %s. wait %s minutes', str(friend[0]), str(waitReconnect))
+			time.sleep(60 * waitReconnect)
+			vk = loginAndGetApi(login, password)
+			continue
+	logger.debug('end writing to file FriendsThatWillBeAdded')
 	
-	logFilesPaths = createLogFiles()
-	
-	print('start writing to file FriendsThatWillBeAdded...')
-	with open(logFilesPaths.get(FRIENDS_WILL_BE_CONST), 'a', encoding='utf-8') as file:
-		file.write('friends amount: ' + str(len(mutualFriendsForAdding)) + '. mutual friends limit = ' + str(MUTUAL_FRIENDS_LIMIT) + '\n')
-		for key in mutualFriendsForAdding :	
-			try :
-				addFriend = vk.users.get(user_id=key)
-				file.write('FRIEND. cnt = ' + str(mutualFriendsForAdding[key]) + '. Info: ' + str(addFriend) + '\n')
-			except (requests.exceptions.ConnectionError, urllib3.exceptions.MaxRetryError, urllib3.exceptions.NewConnectionError) as exp :	
-				waitReconnect = random.randint(3, 5)
-				print('Connection refused! User with id: ' + str(key) + '. wait ' + str(waitReconnect) + ' minutes')
-				time.sleep(60 * waitReconnect)
-				vk = loginAndGetApi(login, password)
-				continue
-	print('end writing to file FriendsThatWillBeAdded')
-
-	print('start writing to file FriendsThatWereAdded...')
-	with open(logFilesPaths.get(FRIENDS_WERE_ADDED_CONST), 'a', encoding='utf-8') as file:
-		file.write('friends amount: ' + str(len(mutualFriendsForAdding)) + '. mutual friends limit = ' + str(MUTUAL_FRIENDS_LIMIT) + '\n')
-		
-	friendsCnt = random.randint(3, 5)
-	i = 0
 	global outgoingRequestsCnt 
 	global currentDay
 	global nextDay
 	global OUTGOING_REQUESTS_DAY_LIMIT 
+	
+	logger.debug('start writing to file FriendsThatWereAdded...')
+	logWereAdded.info('friends amount: %s. mutual friends limit: %s. outgoing requests limit: %s', str(len(mutualFriendsForAdding)), str(MUTUAL_FRIENDS_LIMIT), str(OUTGOING_REQUESTS_DAY_LIMIT))
+		
+	friendsBunchLimit = random.randint(3, 5)
+	i = 0
+
 	for key in mutualFriendsForAdding : 
 		try :
-			addFriend = vk.users.get(user_id=key)
-			vk.friends.add(user_id=key)
+			friend = mutualFriendsForAdding[key]
+			#vk.friends.add(user_id=key)
 			outgoingRequestsCnt += 1
 			if outgoingRequestsCnt == 1 :
-				nextDay = datetime.datetime.today() + datetime.timedelta(days=1)
-				print('Limit: ' + str(OUTGOING_REQUESTS_DAY_LIMIT) + ' for day till: ' + str(nextDay))
+				nextDay = datetime.today() + timedelta(days=1)
+				logger.debug('Limit: %s for day till: %s', str(OUTGOING_REQUESTS_DAY_LIMIT), str(nextDay))
 			if outgoingRequestsCnt >= OUTGOING_REQUESTS_DAY_LIMIT :
-				currentDay = datetime.datetime.today()
+				currentDay = datetime.today()
 				if currentDay < nextDay : 
-					temp = (nextDay - currentDay).seconds
-					print('\tOutgoingRequests limit! wait for ' + str((temp + 100) / 60) + ' minutes. Till' + str(nextDay))
-					time.sleep(temp + 100)
+					waitOutLimit = (nextDay - currentDay).seconds
+					logger.info('Outgoing requests limit! wait for %s minutes. Till %s', str((waitOutLimit + 100) / 60), str(nextDay))
+					time.sleep(waitOutLimit + 100)
 				outgoingRequestsCnt = 0
-			print('\trequest sent. at time: ' + str(datetime.datetime.now()) + '. cnt = ' + str(mutualFriendsForAdding[key]) + ' friend: ' + str(addFriend))
-			with open(logFilesPaths.get(FRIENDS_WERE_ADDED_CONST), 'a', encoding='utf-8') as file:	
-				#file.write('request sent. cnt = ' + str(mutualFriendsForAdding[key]) + ' friend: ' + str(addFriend) + '\n')
-				file.write('request sent. at time: ' + str(datetime.datetime.now()) + '. cnt = ' + str(mutualFriendsForAdding[key]) + ' friend: ' + str(addFriend) + '\n')
-		except vk_api.exceptions.ApiError :
-			print('exception with user. User with id: ' + str(key))
+			logWereAdded.info('Request sent. cnt = %s. friend: %s', str(friend[1]), str(friend[0]))
+		except vk_api.exceptions.ApiError as expA :
+			logger.exception('exception with user. User: %s. Exception: %s', str(friend[0]), expA)
 			continue
 		except (requests.exceptions.ConnectionError, urllib3.exceptions.MaxRetryError, urllib3.exceptions.NewConnectionError) as exp :	
 			waitReconnect = random.randint(3, 5)
-			print('\tConnection refused! User with id: ' + str(key) + '. wait ' + str(waitReconnect) + ' minutes')
+			logger.exception('Connection refused! User: %s. wait %s minutes', str(friend[0]), str(waitReconnect))
 			time.sleep(60 * waitReconnect)
 			vk = loginAndGetApi(login, password)
 			continue		
 		time.sleep(random.randint(33, 59))
 		i += 1
-		if i >= friendsCnt :
-			friendsCnt = random.randint(3, 5)
+		if i >= friendsBunchLimit :
+			friendsBunchLimit = random.randint(3, 5)
 			i = 0
 			randd = random.randint(33, 88)
-			print('wait ' + str(randd) + ' minutes...')
+			logger.info('wait %s minutes...', str(randd))
 			time.sleep(60 * randd)
 
 			
-	print('end writing to file FriendsThatWereAdded')	
+	logger.debug('end writing to file FriendsThatWereAdded')	
 	return len(mutualFriendsForAdding)
 
-login = input('Enter login: ')
-password = input('Enter password: ')
-mutualFriendsCnt = int(input('Enter mutual friends amount: '))
-vk = loginAndGetApi(login, password)
-
-try : 
-	resultedCnt = addPossibleFriendsWithCommonFriends(vk, login, password, mutualFriendsCnt)
-except (requests.exceptions.ConnectionError, urllib3.exceptions.MaxRetryError, urllib3.exceptions.NewConnectionError) as exp :
-	print('Connection refused! rerun adding with mutualFriendsCnt: ' + str(mutualFriendsCnt))
+def main() : 
+	login = input('Enter login: ')
+	password = input('Enter password: ')
+	mutualFriendsCnt = int(input('Enter mutual friends amount: '))
 	vk = loginAndGetApi(login, password)
-	resultedCnt = addPossibleFriendsWithCommonFriends(vk, login, password, mutualFriendsCnt)
-
-while resultedCnt > 0 :
-	if resultedCnt <= 8 :
-		randd = 1
-	elif resultedCnt < 20 :
-		randd = resultedCnt % 8
-	else :
-		randd = random.randint(4, 8)
-	print('wait for ' + str(randd) + ' hours!...')
-	time.sleep(60 * 60 * randd)
-	mutualFriendsCnt += 1
-	print('mutual friends limit = ' + str(mutualFriendsCnt))
+	
 	try : 
 		resultedCnt = addPossibleFriendsWithCommonFriends(vk, login, password, mutualFriendsCnt)
 	except (requests.exceptions.ConnectionError, urllib3.exceptions.MaxRetryError, urllib3.exceptions.NewConnectionError) as exp :
-		print('Connection refused! rerun adding with mutualFriendsCnt: ' + str(mutualFriendsCnt))
+		logger.exception("Connection refused! rerun adding with mutualFriendsCnt: %s Exception: %s", str(mutualFriendsCnt), exp)
 		vk = loginAndGetApi(login, password)
 		resultedCnt = addPossibleFriendsWithCommonFriends(vk, login, password, mutualFriendsCnt)
 		
-input('Please enter to exit...')
+	while resultedCnt > 0 :
+		if resultedCnt <= 8 :
+			randd = random.randint(3, 41)
+		elif resultedCnt < 20 :
+			randd = random.randint(1, 3) * 60
+		else :
+			randd = random.randint(4, 8) * 60
+		logger.debug('Bunch done! wait for %s minutes!...', str(randd))
+		time.sleep(60 * randd)
+		mutualFriendsCnt += 1
+		logger.debug('mutual friends limit = %s', str(mutualFriendsCnt))
+		try : 
+			resultedCnt = addPossibleFriendsWithCommonFriends(vk, login, password, mutualFriendsCnt)
+		except (requests.exceptions.ConnectionError, urllib3.exceptions.MaxRetryError, urllib3.exceptions.NewConnectionError) as exp :
+			logger.exception('Connection refused! rerun adding with mutualFriendsCnt: %s', str(mutualFriendsCnt))
+			vk = loginAndGetApi(login, password)
+			resultedCnt = addPossibleFriendsWithCommonFriends(vk, login, password, mutualFriendsCnt)
+			
+	return
+
+# log files and main logger
+createLogFiles()
+logger = createLogger('main', LOG_PATH(), logging.DEBUG, True)	
+
+# global variables
+OUTGOING_REQUESTS_DAY_LIMIT = 40  
+outgoingRequestsCnt = 0
+currentDay = datetime.today()
+nextDay = currentDay + timedelta(days=1)
+
+try:
+    main()
+except Exception as e:
+    logger.exception("%s", e)
