9252623cfefc97a5dbef4b4a001c390288754cee
Цой Олег Вячеславович <OVTsoy.OUT@sberbank.ru>
2018-08-09 17:02:42 +0300

    m

--- removeAllOutgoingRequests.py
+++ removeAllOutgoingRequests.py
@@ -15,14 +15,15 @@ vk = vk_session.get_api()
 outRequests = vk.friends.getRequests(out=1).get('items')
 
 for outRequest in outRequests :
- 	outFriendFriends = vk.friends.get(user_id=outRequest).get('items')
-	try :
-		logging.info('delete request for user: ' + str(vk.users.get(user_id=outRequest)) + '. User friends cnt: ' + str(len(outFriendFriends)) + '\n')
-		if len(outFriendFriends) >= 1000 :
-			vk.friends.delete(user_id=outRequest)
-	except vk_api.exceptions.ApiError :	
-		logging.info('exception or connection refused. User with id: ' + str(outRequest))
-		continue
+    outFriendFriends = vk.friends.get(user_id=outRequest).get('items')
+    try :
+        logging.info('delete request for user: ' + str(vk.users.get(user_id=outRequest)) + '. User friends cnt: ' + str(len(outFriendFriends)))
+        if len(outFriendFriends) >= 1000 :
+            logging.info('More than 1000')
+            #vk.friends.delete(user_id=outRequest)
+    except vk_api.exceptions.ApiError :	
+        logging.info('exception or connection refused. User with id: ' + str(outRequest))
+        continue
 	
 
 
